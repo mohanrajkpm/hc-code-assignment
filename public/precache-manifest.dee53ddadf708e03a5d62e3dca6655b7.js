a7911a8fe58277a0ace3ba148346eadbb72d87c7
mohananchanto <mohanraj.velusamy@anchanto.com>
2020-09-10 12:47:45 +0530

    Initial commit

---
+++ public/precache-manifest.dee53ddadf708e03a5d62e3dca6655b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78ca3e30c765df77e147086c7ee29da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d59ba7114e346bcbe5e",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "c07a530cb9e0aec6099a",
+    "url": "/static/css/main.a93f0a35.chunk.css"
+  },
+  {
+    "revision": "2d59ba7114e346bcbe5e",
+    "url": "/static/js/2.84a4266b.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/static/js/2.84a4266b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07a530cb9e0aec6099a",
+    "url": "/static/js/main.6d8125a6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);

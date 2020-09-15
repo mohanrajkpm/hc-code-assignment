0001aacac8e1d08895da949f8c2f6abe9b967d20
mohanrajkpm <infomohanrajsoft@gmail.com>
2020-09-15 09:51:39 +0530

    Add comment for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Routes with name for question, role & mapping
   namespace :api do
-	  namespace :v1 do
+    namespace :v1 do
       resources :questions, only: %i[index create show update destroy] do
         collection do
           get :fetch_roles, :fetch_mappings
         end
       end
-	  end
-	end
+    end
+  end
 end
